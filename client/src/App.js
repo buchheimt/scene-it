0991a2c94528e0f3357069ab8884ab93a581e9ec
Tyler <tbuchhei@alumni.nd.edu>
2017-11-21 15:53:51 -0600

    Set up basic react page structure and add NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/navbar';
+import HomePage from './containers/homepage.js';
 
 class App extends Component {
   render() {
+    console.log(this.props)
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Scene It</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route exact path={'/'} component={HomePage} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => (
+  <header className="App-header">
+    <h1 className="App-title">Scene It</h1>
+  </header>
+)
+
+export default NavBar;
