90ebd2ef3364d736ff89808d3ab16a53f7c57d78
Tyler <tbuchhei@alumni.nd.edu>
2017-11-24 21:55:38 -0600

    Add Figaro gem and get basic login working

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'active_model_serializers', '0.10.6'
 gem 'jwt'
 gem 'bcrypt'
+gem 'pry'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
 
   def create
+    binding.pry
     user = User.find_by(email: auth_params[:email])
     if user.authenticate(auth_params[:password])
       jwt = Auth.issue({user: user.id})
+      # binding.pry
       render json: {jwt: jwt}
     else
 
